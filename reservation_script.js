64587abe31c58fc546a71d9042c82ca37977a1d3
John Paul Soria <112761565+johnpaulsoria@users.noreply.github.com>
2023-04-10 15:40:55 +0800

    Add files via upload

---
+++ reservation_script.js
@@ -0,0 +1,38 @@
+function updateTextBox() {
+    // Get the input field and text box elements
+    var inputField = document.getElementById("inputField");
+    var textBox = document.getElementById("textBox");
+
+    // Get the user input and update the text box
+    var userInput = inputField.value;
+    textBox.value += userInput + "\n";
+
+    // Clear the input field
+    inputField.value = "";
+}
+
+function updateTextBox2() {
+    // Get the input field and text box elements
+    var inputField2 = document.getElementById("inputField2");
+    var textBox2 = document.getElementById("textBox2");
+
+    // Get the user input and update the text box
+    var userInput2 = inputField2.value;
+    textBox2.value += userInput2 + "\n";
+
+    // Clear the input field
+    inputField2.value = "";
+}
+
+function updateTextBox3() {
+    // Get the input field and text box elements
+    var inputField3 = document.getElementById("inputField3");
+    var textBox3 = document.getElementById("textBox3");
+
+    // Get the user input and update the text box
+    var userInput3 = inputField3.value;
+    textBox3.value += userInput3 + "\n";
+
+    // Clear the input field
+    inputField3.value = "";
+}
