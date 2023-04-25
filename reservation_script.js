2ad3cf688917e1081ab40bf4612abac08872cd84
John Paul Soria <112761565+johnpaulsoria@users.noreply.github.com>
2023-04-25 14:53:22 +0700

    initial commit

--- reservation_script.js
+++ reservation_script.js
@@ -1,38 +1,20 @@
-function updateTextBox() {
-    // Get the input field and text box elements
-    var inputField = document.getElementById("inputField");
-    var textBox = document.getElementById("textBox");
-
-    // Get the user input and update the text box
-    var userInput = inputField.value;
-    textBox.value += userInput + "\n";
-
-    // Clear the input field
-    inputField.value = "";
-}
-
-function updateTextBox2() {
-    // Get the input field and text box elements
-    var inputField2 = document.getElementById("inputField2");
-    var textBox2 = document.getElementById("textBox2");
-
-    // Get the user input and update the text box
-    var userInput2 = inputField2.value;
-    textBox2.value += userInput2 + "\n";
-
-    // Clear the input field
-    inputField2.value = "";
-}
-
-function updateTextBox3() {
-    // Get the input field and text box elements
-    var inputField3 = document.getElementById("inputField3");
-    var textBox3 = document.getElementById("textBox3");
-
-    // Get the user input and update the text box
-    var userInput3 = inputField3.value;
-    textBox3.value += userInput3 + "\n";
-
-    // Clear the input field
-    inputField3.value = "";
-}
+function submitData() {
+    const fullName = document.getElementById("fullName").value;
+    const phoneNumber = document.getElementById("phoneNumber").value;
+    const emailAdd = document.getElementById("emailAdd").value;
+    const address = document.getElementById("address").value;
+    const destiAdd = document.getElementById("destiAdd").value;
+    const flightSched = document.getElementById("flightSched").value;
+    const pay = document.getElementById("pay").value;
+  
+  
+    console.log(`Name: ${fullName}`);
+    console.log(`Phone: ${phoneNumber}`);
+    console.log(`Email: ${emailAdd}`);
+    console.log(`Address: ${address}`);
+    console.log(`Destination Address: ${destiAdd}`);
+    console.log(`Flight Schedule: ${flightSched}`);
+    console.log(`Payment Method: ${pay}`);
+    console.log("");
+    alert("Registration Successfully");
+  }
